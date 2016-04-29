51724c2684bb098878cdfab0c7f2fc2e684d4d1f
Jooyoun Lee <jjooyoun@gmail.com>
2016-04-29 01:05:17 -0700

    bugfix
    bugfix

--- mocking up/test.php
+++ mocking up/test.php
@@ -8,6 +8,7 @@
 		shell_exec('mkdir /var/www/html/json');
 		shell_exec('cp /home/ubuntu/COVERT/covert_dist/app_repo/bundle/analysis/merged/*.xml /var/www/html/xml');
 		shell_exec('python /var/www/html/Parser.py');
+		shell_exec('rm /var/www/html/InteractionsOutput.json');
 		shell_exec('python /var/www/html/Interactions.py');
 		echo "OK";
 	endif;
